6f7184c4e91762dcb193b2c6ea7370c514ada990
The Rissenator <mrissen@centriq.com>
2021-06-29 23:59:40 -0500

    Changes to setup using the new easier way

--- MSALClientApp/MSALClientApp/AzureAuthenticationStuff.cs
+++ MSALClientApp/MSALClientApp/AzureAuthenticationStuff.cs
@@ -10,11 +10,8 @@ namespace ConsoleApp17
 {
     public static class AzureAuthenticationStuff
     {
-        public static string ClientID = "d824b26a-e490-4990-aa45-1add6c11f80f";
-        public static string TenantID = "1ee67c73-1eec-4dc1-9105-cbd0193a0915";
-        public static string[] Scopes = { "user.read", "api://d824b26a-e490-4990-aa45-1add6c11f80f/user_impersonation" };
+        public static string[] Scopes = { "user.read" };
         public static string Redirect = "https://login.microsoftonline.com/common/oauth2/nativeclient";
-        public static string Url = "https://centriqtoons.azurewebsites.net/toons";
         public static string GetAuthToken()
         {
             IPublicClientApplication app= PublicClientApplicationBuilder
